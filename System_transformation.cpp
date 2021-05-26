94c8c4adc11aab5ecaedcfc9c93f6b47d74966c9
renhai1 <1457481392@qq.com>
2021-05-26 13:37:07 +0800

    数制转换正确通过（哭了考虑不周全）

--- System_transformation.cpp
+++ System_transformation.cpp
@@ -8,7 +8,7 @@ int main()
     int len=number1.length();
     int* number2=new int[len];
     //讲字符串转换为整型数组
-    for(int i=len-1;i>=0;i--)
+    for(int i=0;i<len;i++)
     {
         int x;
         stringstream ss;
@@ -16,18 +16,34 @@ int main()
         ss >> x;
         number2[i]=x;
     }
+
     //按照高次幂到底次幂的顺序输出
-    for(int j=len-1;j>0;j--)
+    int t=0;
+    for(int j=len-1;j>=0;j--)
     {
-        if(number2[j]!=0)
+        if(number2[t]!=0)
         {
-            cout<<number2[j]<<"*"<<scale<<"^"<<j<<"+";
+            cout<<number2[t]<<"*"<<scale<<"^"<<j;
 
         }
-    }
-    if(number2[0]!=0)
-    {
-        cout<<number2[0]<<"*"<<scale<<"^0";
+        bool flag=true;
+        for(int k=t+1;k<len;k++)
+        {
+            if(number2[k]!=0)
+            {
+                flag=false;
+                break;
+            }
+        }
+        if(flag)
+        {
+           return 0;
+        }
+        if(number2[t]!=0&&j>0)
+        {
+            cout<<"+";
+        }
+        t++;
     }
     return 0;
 }
